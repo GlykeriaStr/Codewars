c946b777abb177604d89c4ca78c8550fc93173ec
Glykeria Stravodimou <strglykeria@gmail.com>
2021-05-06 15:59:14 +0300

    Raising error when there is no needle

--- A_Needle_In_the_Haystack/lib/needle.rb
+++ A_Needle_In_the_Haystack/lib/needle.rb
@@ -1,3 +1,4 @@
 def find_needle(array)
+  raise 'No needle found' unless array.include?('needle')
   array.each_with_index { |word, index| return "found the needle at position #{index}" if word == "needle" }
 end
--- A_Needle_In_the_Haystack/spec/needle_spec.rb
+++ A_Needle_In_the_Haystack/spec/needle_spec.rb
@@ -20,4 +20,8 @@ describe  'find_needle' do
   it 'returns "found the needle at position 0" when passed ["needle", "hay", "hay"] ' do
     expect(find_needle(['needle', 'hay',"hay"])).to eq('found the needle at position 0')
   end
+
+  it 'returns "No needle found" when passed ["hay", "hay"]' do
+    expect{ find_needle(["hay", "hay"])}.to raise_error "No needle found"
+  end
 end
