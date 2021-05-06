74e8c1b098eda378a7b9bef97f05cb0df1c567eb
Glykeria Stravodimou <strglykeria@gmail.com>
2021-05-06 14:03:28 +0300

    Needle at position 2 / hardcoded

--- A_Needle_In_the_Haystack/lib/needle.rb
+++ A_Needle_In_the_Haystack/lib/needle.rb
@@ -1,3 +1,9 @@
 def find_needle(array)
-  array.length == 1 ? "found the needle at position 0" : "found the needle at position 1"
+  if array.length == 1 
+    "found the needle at position 0" 
+  elsif array.length == 2
+    "found the needle at position 1"
+  else
+    "found the needle at position 2"
+  end
 end
--- A_Needle_In_the_Haystack/spec/needle_spec.rb
+++ A_Needle_In_the_Haystack/spec/needle_spec.rb
@@ -8,4 +8,8 @@ describe  'find_needle' do
   it 'returns "found the needle at position 1" when passed ["hay","needle"] ' do
     expect(find_needle(['hay','needle'])).to eq('found the needle at position 1')
   end
+
+  it 'returns "found the needle at position 2" when passed ["hay","hay","needle"] ' do
+    expect(find_needle(['hay', 'hay', 'needle'])).to eq('found the needle at position 2')
+  end
 end
