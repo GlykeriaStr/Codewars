fdc8f455efa6defcb71faa116579dadb6eb6ce73
Glykeria Stravodimou <strglykeria@gmail.com>
2021-05-11 14:47:19 +0100

    Passing position 0 and position 1, hardcoded

--- A_Needle_In_the_Haystack/Python/needle.py
+++ A_Needle_In_the_Haystack/Python/needle.py
@@ -1,2 +1,2 @@
 def find_needle(list):
-  return "Found needle at position 0"
+  return "Found needle at position 0" if len(list) == 1 else "Found needle at position 1"
--- A_Needle_In_the_Haystack/Python/test_needle.py
+++ A_Needle_In_the_Haystack/Python/test_needle.py
@@ -3,3 +3,4 @@ from needle import find_needle
 
 def test_find_needle():
   assert find_needle(["needle"]) == 'Found needle at position 0'
+  assert find_needle(["hay", "needle"]) == 'Found needle at position 1'
