bb61b7d7d748ad7a900d2f8f2175d09f041bf116
Glykeria Stravodimou <strglykeria@gmail.com>
2021-05-11 14:57:10 +0100

    Refactored using a loop and interpolation

--- A_Needle_In_the_Haystack/Python/needle.py
+++ A_Needle_In_the_Haystack/Python/needle.py
@@ -1,7 +1,5 @@
 def find_needle(list):
-  if len(list) == 1: 
-    return "Found needle at position 0"
-  elif len(list) == 2:
-    return "Found needle at position 1"
-  else:
-    return "Found needle at position 2"
+  for i in list:
+    if i == 'needle':
+      return f"Found needle at position {list.index('needle')}"
+  
