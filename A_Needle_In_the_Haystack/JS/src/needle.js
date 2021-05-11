c227eb38ad0ab50bd79222f0e22914482e3776e4
Glykeria Stravodimou <strglykeria@gmail.com>
2021-05-11 13:41:43 +0100

    Passing test when needle in position 1, hardcoded

--- A_Needle_In_the_Haystack/JS/src/needle.js
+++ A_Needle_In_the_Haystack/JS/src/needle.js
@@ -1,5 +1,9 @@
 function findNeedle(array) {
-  return "Found needle at position 0";
+  if (array[0] === "needle") {
+    return "Found needle at position 0";
+  } else {
+    return "Found needle at position 1";
+  }
 }
 
 module.exports = findNeedle;
--- A_Needle_In_the_Haystack/JS/test/needle.test.js
+++ A_Needle_In_the_Haystack/JS/test/needle.test.js
@@ -3,3 +3,7 @@ const findNeedle = require("../src/needle.js");
 test('returns "found needle at position 0" when passed ["needle"]', () => {
   expect(findNeedle(["needle"])).toEqual("Found needle at position 0");
 });
+
+test('returns position 1 when passed ["hay","needle"]', () => {
+  expect(findNeedle(["hay", "needle"])).toEqual("Found needle at position 1");
+});
